a90258c7e62f7c8968a28f9b67cb6ad5dbf4be68
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2025-04-07 17:16:20 +0200

    feat (seed): ajout seed

---
+++ vim-chess-api/prisma/seed.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import * as process from 'node:process';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const adminRole = await prisma.role.upsert({
+    where: { role: 'Admin' },
+    update: {},
+    create: {
+      role: 'Admin',
+    },
+  });
+
+  const userRole = await prisma.role.upsert({
+    where: { role: 'User' },
+    update: {},
+    create: {
+      role: 'User',
+    },
+  });
+
+  const hashedPasswordAdminUser = await bcrypt.hash('kantin', 10);
+
+  const adminUser = await prisma.user.upsert({
+    where: { email: 'kantin.fagn@gmail.com' },
+    update: {},
+    create: {
+      lastname: 'Fagniart',
+      firstname: 'Kantin',
+      email: 'kantin.fagn@gmail.com',
+      username: 'Kavtiv',
+      country: 'Algeria',
+      password: hashedPasswordAdminUser,
+      roleId: adminRole.id,
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    console.log('Seeding completed');
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
